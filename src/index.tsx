02a712e8727b249e8c18558ac0b0311fc37d8edd
root <root@DESKTOP-2034I7J.localdomain>
2020-10-23 13:56:28 +0300

    Clear Main

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-
-import { ClickCounter } from "./ClickCounter";
-
-render(<ClickCounter start={1} />, document.getElementById("root"));
